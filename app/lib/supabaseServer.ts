8b24453d06e2058e4b926fffc9ac7378cd3f9250
oscar-gamestorm <ejumudooscar35@gmail.com>
2025-12-31 15:04:41 -0800

    trigger redeploy

---
+++ app/lib/supabaseServer.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export async function getSupabaseServer() {
+  const cookieStore = await cookies(); // await required
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set() {},
+        remove() {}
+      }
+    }
+  );
+}
